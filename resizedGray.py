855252cca7b961f1d5512d44573c79c4b31f379e
Anujit Ganguly <gangulyanujit@gmail.com>
2019-08-31 19:05:06 +0530

    Create resizedGray.py

---
+++ resizedGray.py
@@ -0,0 +1,16 @@
+from PIL import Image 
+import os
+
+path = ('elefante/')
+dirs = os.listdir( path )
+
+def grey():
+    for item in dirs:
+        if os.path.isfile(path+item):
+            im = Image.open(path+item)
+            f, e = os.path.splitext(path+item)
+            img = im.resize((100, 100), Image.ANTIALIAS)
+            imConv = img.convert('1') 
+            imConv.save(f + 'result.jpg','JPEG', quality = 100)
+            
+grey()
